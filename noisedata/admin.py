ec6acd1c76440f8407c34e3057bdc484c9b0245a
Lumiukko <makko@brainscorch.net>
2017-02-18 22:42:49 +0100

    Added first draft of NoiseData model.
    * added pycharm ide files to gitignore
    * outcommented unicode stuff

--- noisedata/admin.py
+++ noisedata/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 from .models import Dummy
+from .models import NoiseData
+
 default_site = admin.site
 
 default_site.register(Dummy)
+default_site.register(NoiseData)
--- noisedata/models.py
+++ noisedata/models.py
@@ -1,8 +1,25 @@
 from django.db import models
+from django.contrib.gis.db import models
+
+class NoiseData(models.Model):
+    center = models.PointField()
+    elevation = models.FloatField()
+    bounding_box = models.GeometryField()
+    # TODO: PostGIS supports RasterField, which we could use instead of GeometryField.
+
+    noise_mean_day = models.FloatField()
+    noise_mean_evening = models.FloatField()
+    noise_mean_night = models.FloatField()
+    noise_weighted_24h = models.FloatField()
+    noise_mean_24h = models.FloatField()
+
+
+
 
 class Dummy(models.Model):
     name = models.CharField(max_length=150)
     position = models.FloatField()
 
-    def __unicode__(self):
-        return unicode(self.name)
+    # Deprecated in Python 3. All Strings are Unicode now.
+    # def __unicode__(self):
+    #    return unicode(self.name)
